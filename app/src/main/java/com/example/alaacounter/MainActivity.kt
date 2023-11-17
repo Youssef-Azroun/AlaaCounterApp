07abc320b7c16830d18dd9682192da996de3ab0a
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2023-11-17 03:12:18 +0100

    fixed main

--- app/src/main/java/com/example/alaacounter/MainActivity.kt
+++ app/src/main/java/com/example/alaacounter/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,22 +30,47 @@ class MainActivity : AppCompatActivity() {
             val email = editTextEmail.text.toString().trim()
             val password = editTextPassword.text.toString().trim()
 
-            // Implement your login logic, for example, using Firebase Authentication
-            // If login is successful, navigate to MainFunctions activity
-            if (loginSuccessful(email, password)) {
-                val intent = Intent(this, MainFunctions::class.java)
-                startActivity(intent)
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                loginWithEmailAndPassword(email, password) { loginResult ->
+                    if (loginResult) {
+                        // Login successful, navigate to MainFunctions activity
+                        val intent = Intent(this, MainFunctions::class.java)
+                        startActivity(intent)
+                    } else {
+                        // Handle login failure, show a toast or appropriate message
+                        Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
             } else {
-                // Handle login failure, show a toast or appropriate message
-                Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+                // Show a toast if email or password is empty
+                Toast.makeText(this, "Please enter both email and password", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun loginSuccessful(email: String, password: String): Boolean {
-        // Implement your login logic here, for example, using Firebase Authentication
-        // Return true if login is successful, otherwise return false
-        // Note: Replace this with your actual login implementation
-        return true
+    private fun loginWithEmailAndPassword(email: String, password: String, callback: (Boolean) -> Unit) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Login successful
+                    val user = FirebaseAuth.getInstance().currentUser
+                    if (user != null) {
+                        // Optionally, you can check additional conditions here
+                        // such as verifying the email or any custom user attributes
+                        callback.invoke(true)
+                    } else {
+                        callback.invoke(false)
+                    }
+                } else {
+                    // If login fails, handle the error
+                    Toast.makeText(
+                        this,
+                        "Login failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    callback.invoke(false)
+                }
+            }
     }
 }
